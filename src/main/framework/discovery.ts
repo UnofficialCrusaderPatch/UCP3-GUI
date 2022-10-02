2f0eb61af1c5fd453f3c82bbdc76abaf4e298ee5
gynt <gynt@users.noreply.github.com>
2022-10-02 23:28:39 +0200

    rewrote discovery extensions to functional style

--- src/main/framework/discovery.ts
+++ src/main/framework/discovery.ts
@@ -1,101 +1,74 @@
 import fs from 'fs';
 import yaml from 'yaml';
 
-import { Definition } from '../../common/config/common';
+import { Definition, Extension } from '../../common/config/common';
+
+type ExtensionExtra = Extension & { folder: string };
 
 const localeSensitiveFields = ['description', 'text', 'tooltip'];
 const localeRegExp = new RegExp('^\\s*{{(.*)}}\\s*$');
 
-export class Extension {
-  name: string;
-
-  version: string;
-
-  type: string;
-
-  folder: string;
-
-  ui: object[];
-
-  definition: Definition;
-
-  config: unknown;
-
-  constructor(
-    name: string,
-    version: string,
-    type: string,
-    folder: string,
-    def: Definition
-  ) {
-    this.name = name;
-    this.version = version;
-    this.type = type;
-    this.folder = folder;
-    this.ui = [];
-    this.definition = def;
-    this.config = {};
+function readUISpec(ext: ExtensionExtra): void {
+  if (fs.existsSync(`${ext.folder}/ui.yml`)) {
+    ext.ui = yaml.parse(
+      fs.readFileSync(`${ext.folder}/ui.yml`, { encoding: 'utf-8' })
+    );
+  } else {
+    ext.ui = [];
   }
+}
 
-  readUISpec(): void {
-    if (fs.existsSync(`${this.folder}/ui.yml`)) {
-      this.ui = yaml.parse(
-        fs.readFileSync(`${this.folder}/ui.yml`, { encoding: 'utf-8' })
-      );
-    }
+function readConfig(ext: ExtensionExtra): void {
+  if (fs.existsSync(`${ext.folder}/config.yml`)) {
+    ext.config = yaml.parse(
+      fs.readFileSync(`${ext.folder}/config.yml`, { encoding: 'utf-8' })
+    );
+  } else {
+    ext.config = {};
   }
+}
 
-  readConfig(): void {
-    if (fs.existsSync(`${this.folder}/config.yml`)) {
-      this.config = yaml.parse(
-        fs.readFileSync(`${this.folder}/config.yml`, { encoding: 'utf-8' })
-      );
-    }
-  }
+function setLocale(ext: ExtensionExtra, language: string): void {
+  function changeLocale(
+    locale: { [key: string]: string },
+    obj: { [key: string]: unknown }
+  ): void {
+    localeSensitiveFields.forEach((field) => {
+      if (typeof obj[field] === 'string') {
+        const search = localeRegExp.exec(obj[field] as string);
 
-  setLocale(language: string): void {
-    function changeLocale(
-      locale: { [key: string]: string },
-      obj: { [key: string]: unknown }
-    ): void {
-      localeSensitiveFields.forEach((field) => {
-        if (typeof obj[field] === 'string') {
-          const search = localeRegExp.exec(obj[field] as string);
-
-          if (search !== undefined && search !== null) {
-            const keyword = search[1];
-            const loc = locale[keyword];
-            if (loc !== undefined) {
-              obj[field] = loc;
-            }
+        if (search !== undefined && search !== null) {
+          const keyword = search[1];
+          const loc = locale[keyword];
+          if (loc !== undefined) {
+            obj[field] = loc;
           }
         }
-        if (typeof obj[field] === 'object') {
-          changeLocale(locale, obj[field] as { [key: string]: unknown });
-        }
-      });
-    }
-
-    if (fs.existsSync(`${this.folder}/locale`)) {
-      if (fs.existsSync(`${this.folder}/locale/${language}.json`)) {
-        const locale = JSON.parse(
-          fs.readFileSync(`${this.folder}/locale/${language}.json`, {
-            encoding: 'utf-8',
-          })
-        );
-
-        this.ui.forEach((uiElement) => {
-          changeLocale(locale, uiElement as { [key: string]: unknown });
-        });
       }
+      if (typeof obj[field] === 'object') {
+        changeLocale(locale, obj[field] as { [key: string]: unknown });
+      }
+    });
+  }
+
+  if (fs.existsSync(`${ext.folder}/locale`)) {
+    if (fs.existsSync(`${ext.folder}/locale/${language}.json`)) {
+      const locale = JSON.parse(
+        fs.readFileSync(`${ext.folder}/locale/${language}.json`, {
+          encoding: 'utf-8',
+        })
+      );
+
+      ext.ui.forEach((uiElement) => {
+        changeLocale(locale, uiElement as { [key: string]: unknown });
+      });
     }
   }
 }
 
 const Discovery = {
-  Extension,
   discoverExtensions: (gameFolder: string) => {
-    const currentLocale = 'English'; // Dummy location for this code
+    const currentLocale = 'English'; // Dummy location for ext code
 
     const moduleDir = `${gameFolder}/ucp/modules`;
     const modDirEnts = fs.existsSync(moduleDir)
@@ -121,21 +94,28 @@ const Discovery = {
             ? `${gameFolder}/ucp/modules/${d.name}`
             : `${gameFolder}/ucp/plugins/${d.name}`;
 
-        const def = yaml.parse(
+        const definition = yaml.parse(
           fs.readFileSync(`${folder}/definition.yml`, { encoding: 'utf-8' })
         );
-        const { name, version } = def;
+        const { name, version } = definition;
 
-        def.dependencies = def.depends || [];
+        definition.dependencies = definition.depends || [];
 
-        const ext = new Extension(name, version, type, folder, def);
-        ext.readUISpec();
-        ext.setLocale(currentLocale);
-        ext.readConfig();
+        const ext = {
+          name,
+          version,
+          type,
+          folder,
+          definition,
+        } as unknown as ExtensionExtra;
+        readUISpec(ext);
+        setLocale(ext, currentLocale);
+        readConfig(ext);
 
         return ext;
       });
   },
 };
 
+// eslint-disable-next-line import/prefer-default-export
 export { Discovery };

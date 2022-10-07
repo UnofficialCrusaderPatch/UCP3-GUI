20ccf5150f91a6b4648761903741b47ee3831bb8
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2022-10-08 01:39:00 +0200

    [RECENT_FOLDERS] small restructure + recent folders object

---
+++ src/renderer/utils/dialog-util.ts
@@ -0,0 +1,22 @@
+import { dialog } from "@tauri-apps/api";
+
+export async function showInfo(message: string, title?: string) {
+    await dialog.message(message, {
+        title: title || undefined,
+        type: 'info',
+    })
+}
+
+export async function showWarning(message: string, title?: string) {
+    await dialog.message(message, {
+        title: title || undefined,
+        type: 'warning',
+    })
+}
+
+export async function showError(message: string, title?: string) {
+    await dialog.message(message, {
+        title: title || undefined,
+        type: 'error',
+    })
+}

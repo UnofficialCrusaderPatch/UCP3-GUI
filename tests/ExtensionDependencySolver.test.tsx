eae4e37cbdfcd4f8342b5c259824912d1cdc6b15
gynt <gynt@users.noreply.github.com>
2022-10-13 21:45:39 +0200

    add old test files from electron branch

---
+++ tests/ExtensionDependencySolver.test.tsx
@@ -0,0 +1,38 @@
+import { describe, expect, test } from 'vitest';
+import YAML from 'yaml';
+import ExtensionDependencySolver from '../src/common/config/ExtensionDependencySolver';
+
+const test1Yaml = `
+- name: extension1
+  definition:
+   dependencies: [extension6]
+- name: extension2
+  definition:
+    dependencies: []
+- name: extension3
+  definition:
+    dependencies: [extension2]
+- name: extension4
+  definition:
+    dependencies: [extension3, extension1]
+- name: extension5
+  definition:
+    dependencies: [extension2]
+- name: extension6
+  definition:
+    dependencies: [extension5]
+`;
+
+const test1 = new ExtensionDependencySolver(YAML.parse(test1Yaml)).solve();
+
+describe('Test 1: Dependency order solving', () => {
+  test('result should be ?', () => {
+    expect(test1).toEqual([
+      ['extension2'],
+      ['extension3', 'extension5'],
+      ['extension6'],
+      ['extension1'],
+      ['extension4'],
+    ]);
+  });
+});

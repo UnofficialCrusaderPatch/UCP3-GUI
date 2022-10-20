f4113ba4d9a895b64b48b07681d36804496d7ff6
gynt <gynt@users.noreply.github.com>
2022-10-21 00:40:50 +0200

    fix missing option entries if category not defined

--- src/renderer/fakeBackend.ts
+++ src/renderer/fakeBackend.ts
@@ -24,6 +24,7 @@ import semver from 'semver';
 import { dialog } from '@tauri-apps/api';
 import { UCP3_REPOS_MACHINE_TOKEN } from '../main/versions/github';
 import {
+  DisplayConfigElement,
   Extension,
   OptionEntry,
   SectionDescription,
@@ -198,23 +199,30 @@ export const ucpBackEnd = {
   },
 
   optionEntriesToHierarchical(uiCollection: OptionEntry[]): SectionDescription {
-    const result = { elements: [], sections: {} };
+    const result: {
+      elements: DisplayConfigElement[];
+      sections: { [key: string]: SectionDescription };
+    } = { elements: [], sections: {} };
 
     uiCollection.forEach((ui) => {
       if (ui.category !== undefined) {
-        let e: { elements: object[]; sections: { [key: string]: object } } =
-          result;
+        let e: {
+          elements: DisplayConfigElement[];
+          sections: { [key: string]: SectionDescription };
+        } = result;
         ui.category.forEach((cat: string) => {
           if (e.sections[cat] === undefined) {
             e.sections[cat] = { elements: [], sections: {} };
           }
           e = e.sections[cat] as {
-            elements: object[];
-            sections: { [key: string]: object };
+            elements: DisplayConfigElement[];
+            sections: { [key: string]: SectionDescription };
           };
         });
         const f = e;
-        f.elements.push(ui);
+        f.elements.push(ui as unknown as DisplayConfigElement);
+      } else {
+        result.elements.push(ui as unknown as DisplayConfigElement);
       }
     });
 

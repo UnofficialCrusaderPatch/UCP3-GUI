44c3f7b142e6b55dd0c0da6dfb5d36058fd22edd
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2022-10-24 01:09:16 +0200

    [CONFIG] likely finished rust part of config move

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,4 +1,7 @@
-use std::{sync::{Mutex, MutexGuard}, path::PathBuf};
+use std::{
+    path::PathBuf,
+    sync::{Mutex, MutexGuard},
+};
 
 use tauri::{AppHandle, Manager, State};
 
@@ -7,17 +10,18 @@ use tauri::{AppHandle, Manager, State};
 const BASE_FOLDER: &str = "UnofficialCrusaderPatch3";
 
 // will panic if not present, but state should exists
-pub fn do_with_mutex_state<T: std::marker::Send + 'static>(
+pub fn do_with_mutex_state<T: std::marker::Send + 'static, F>(
     app_handle: &AppHandle,
-    do_with_state: fn(app_handle: &AppHandle, state: &mut MutexGuard<T>),
-) {
+    mut do_with_state: F,
+) where
+    F: FnMut(&mut MutexGuard<T>) -> (),
+{
     let state: State<Mutex<T>> = app_handle.state();
-    do_with_state(app_handle, &mut state.lock().unwrap());
+    do_with_state(&mut state.lock().unwrap());
 }
 
-
 pub fn get_roaming_folder_path() -> PathBuf {
-    let mut data_path = tauri::api::path::data_dir().unwrap();  // fail if not there
+    let mut data_path = tauri::api::path::data_dir().unwrap(); // fail if not there
     data_path.push(BASE_FOLDER);
     data_path
 }

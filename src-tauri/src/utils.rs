02354187c959b3640ddb5a3831ec6bf2b1d7a7e0
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2022-10-30 23:42:31 +0100

    [ZIP] created rust code and bindings

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,9 +1,9 @@
 use std::{
-    path::PathBuf,
+    path::{Path, PathBuf},
     sync::{Mutex, MutexGuard},
 };
 
-use tauri::{AppHandle, Manager, State};
+use tauri::{AppHandle, Error, Manager, State};
 
 // CONSTANTS
 
@@ -24,4 +24,24 @@ pub fn get_roaming_folder_path() -> PathBuf {
     let mut data_path = tauri::api::path::data_dir().unwrap(); // fail if not there
     data_path.push(BASE_FOLDER);
     data_path
+}
+
+pub fn get_allowed_path<'a>(app_handle: &AppHandle, path: &'a str) -> Result<&'a Path, Error> {
+    let path_object = Path::new(path);
+    if app_handle.fs_scope().is_allowed(path_object) {
+        Ok(path_object)
+    } else {
+        Err(Error::PathNotAllowed(path_object.to_path_buf()))
+    }
+}
+
+pub fn get_allowed_path_with_string_error<'a>(
+    app_handle: &AppHandle,
+    path: &'a str,
+) -> Result<&'a Path, String> {
+    let result = get_allowed_path(app_handle, path);
+    match result {
+        Ok(path) => Ok(path),
+        Err(error) => Err(error.to_string()),
+    }
 }

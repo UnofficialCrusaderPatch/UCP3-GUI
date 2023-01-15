0b4c02d41ec1a263126fba51170a19808c2c55c0
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2023-01-15 04:23:29 +0100

    [LOGGING] added logging base to Rust backend
    - currently still missing API for JS
    - do not forget to have a catch all for all frontend errors

---
+++ src-tauri/src/constants.rs
@@ -0,0 +1,20 @@
+// General
+
+pub const BASE_FOLDER: &str = "UnofficialCrusaderPatch3";
+
+// Gui-Config
+
+pub const NUMBER_OF_RECENT_FOLDERS: usize = 10;
+pub const CONFIG_FILE_NAME: &str = "config.json";
+pub const MESSAGE_TITLE: &str = "GUI-Configuration";
+pub const LANGUAGE_CHANGE_EVENT: &str = "language-change";
+
+pub const LOG_LEVEL_DEFAULT: &str = "INFO";
+
+// logging
+
+pub const LOG_PATTERN: &str = "{d} {l} {t} - {m}{n}"; // default pattern copied from file
+pub const LOG_FOLDER: &str = "logs";
+pub const LOG_FILE_PATTERN: &str = "ucp-gui.{}.log";
+pub const LOG_COUNT: u32 = 25;
+pub const LOG_BYTE_SIZE: u64 = 5 * 1024; // 5KB as max log file size to roll

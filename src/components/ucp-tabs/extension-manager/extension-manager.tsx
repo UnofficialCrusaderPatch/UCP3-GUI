29d29f44e5db1ba291a717147f1cacfb953ac4f7
gynt <gynt@users.noreply.github.com>
2023-07-18 00:17:53 +0200

    moved extension adding/removing logic to its own functions

--- src/components/ucp-tabs/extension-manager/extension-manager.tsx
+++ src/components/ucp-tabs/extension-manager/extension-manager.tsx
@@ -20,6 +20,11 @@ import { info } from 'util/scripts/logging';
 
 import ExtensionElement from './extension-element';
 import { propagateActiveExtensionsChange } from '../helpers';
+import {
+  addExtensionToExplicityActivatedExtensions,
+  moveExtension,
+  removeExtensionFromExplicitlyActivatedExtensions,
+} from './extensions-state';
 
 export default function ExtensionManager() {
   const extensions = useExtensions();
@@ -85,45 +90,18 @@ export default function ExtensionManager() {
       clickCallback={(event) => {
         // TODO: include a check where it checks whether the right version of an extension is available and selected (version dropdown box)
 
-        const dependencyExtensionNames = eds.dependenciesFor(ext.name).flat();
+        const newExtensionState = addExtensionToExplicityActivatedExtensions(
+          extensionsState,
+          eds,
+          extensionsByName,
+          extensionsByNameVersionString,
+          ext
+        );
 
-        const dependencies: Extension[] = dependencyExtensionNames
-          .filter(
-            (v: string) =>
-              extensionsState.activeExtensions
-                .map((e: Extension) => e.name)
-                .indexOf(v) === -1
-          )
-          .map((v: string) => {
-            if (extensionsByName[v] !== undefined) {
-              return extensionsByName[v];
-            }
-            throw new Error();
-          }) //           .filter((e: Extension) => dependencyExtensionNames.indexOf(e.name) !== -1)
-          .reverse();
+        const newActiveExtensions = newExtensionState.activeExtensions;
 
-        const remainder = extensionsState.activeExtensions
-          .flat()
-          .map((e: Extension) => `${e.name}-${e.version}`)
-          .filter(
-            (es) =>
-              dependencies
-                .map((e: Extension) => `${e.name}-${e.version}`)
-                .indexOf(es) === -1
-          )
-          .map((es: string) => extensionsByNameVersionString[es]);
-
-        const final = [...dependencies, ...remainder];
-
-        const localEDS = new ExtensionDependencySolver(final);
-        info(localEDS.solve());
-        const order = localEDS
-          .solve()
-          .reverse()
-          .map((a: string[]) => a.map((v: string) => extensionsByName[v]));
-
-        onActiveExtensionsUpdate(final);
-        propagateActiveExtensionsChange(final, {
+        onActiveExtensionsUpdate(newActiveExtensions);
+        propagateActiveExtensionsChange(newActiveExtensions, {
           setActiveExtensions,
           extensionsState,
           setExtensionsState,
@@ -133,18 +111,8 @@ export default function ExtensionManager() {
           setConfigurationWarnings,
           setConfigurationLocks,
         });
-        setExtensionsState({
-          // allExtensions: extensionsState.allExtensions,
-          activatedExtensions: [...extensionsState.activatedExtensions, ext],
-          //  activeExtensions: final,
-          installedExtensions: extensionsState.installedExtensions.filter(
-            (e: Extension) =>
-              final
-                .map((ex: Extension) => `${ex.name}-${ex.version}`)
-                .indexOf(`${e.name}-${e.version}`) === -1
-          ),
-        });
-        setActiveExtensions(final);
+        setExtensionsState(newExtensionState);
+        setActiveExtensions(newActiveExtensions);
       }}
       moveCallback={(event: { type: 'up' | 'down' }) => {}}
       revDeps={revDeps[ext.name].filter(
@@ -172,53 +140,24 @@ export default function ExtensionManager() {
         movability={movability}
         buttonText={t('gui-general:deactivate')}
         clickCallback={(event) => {
-          const relevantExtensions = new Set(
-            extensionsState.activatedExtensions
-              .filter(
-                (e) => `${e.name}-${e.version}` !== `${ext.name}-${ext.version}`
-              )
-              .map((e: Extension) => eds.dependenciesFor(e.name).flat())
-              .flat()
-          );
-
-          // extensionsState.activeExtensions.filter((e: Extension) => relevantExtensions.has(e));
-          const ae = extensionsState.activeExtensions.filter((e) =>
-            relevantExtensions.has(e.name)
-          );
+          const newExtensionState =
+            removeExtensionFromExplicitlyActivatedExtensions(
+              extensionsState,
+              eds,
+              extensions,
+              ext
+            );
+          const ae = newExtensionState.activeExtensions;
           onActiveExtensionsUpdate(ae);
-          setExtensionsState({
-            allExtensions: extensionsState.allExtensions,
-            activatedExtensions: extensionsState.activatedExtensions.filter(
-              (e) => `${e.name}-${e.version}` !== `${ext.name}-${ext.version}`
-            ),
-            activeExtensions: ae,
-            installedExtensions: extensions
-              .filter((e: Extension) => !relevantExtensions.has(e.name))
-              .sort((a: Extension, b: Extension) =>
-                a.name.localeCompare(b.name)
-              ),
-          } as unknown as ExtensionsState);
+          setExtensionsState(newExtensionState);
           setActiveExtensions(ae);
         }}
         moveCallback={(event: { name: string; type: 'up' | 'down' }) => {
-          const { name, type } = event;
-          const aei = extensionsState.activeExtensions
-            .map((e) => e.name)
-            .indexOf(name);
-          const element = extensionsState.activeExtensions[aei];
-          let newIndex = type === 'up' ? aei - 1 : aei + 1;
-          newIndex = newIndex < 0 ? 0 : newIndex;
-          newIndex =
-            newIndex > extensionsState.activeExtensions.length - 1
-              ? extensionsState.activeExtensions.length - 1
-              : newIndex;
-          extensionsState.activeExtensions.splice(aei, 1);
-          extensionsState.activeExtensions.splice(newIndex, 0, element);
-          onActiveExtensionsUpdate(extensionsState.activeExtensions);
-          setExtensionsState({
-            activeExtensions: extensionsState.activeExtensions,
-          } as unknown as ExtensionsState);
-          setActiveExtensions(extensionsState.activeExtensions);
+          const newExtensionsState = moveExtension(extensionsState, event);
+
+          onActiveExtensionsUpdate(newExtensionsState.activeExtensions);
+          setExtensionsState(newExtensionsState);
+          setActiveExtensions(newExtensionsState.activeExtensions);
         }}
         revDeps={revDeps[ext.name].filter(
           (e: string) =>

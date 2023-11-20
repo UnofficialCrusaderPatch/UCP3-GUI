0c426a0182b4b6f0eaf4dc1cb59c6088b9313af3
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2023-11-20 01:21:37 +0100

    [GENERAL_CLEAN_UP] reworked base structure of extension manager tab

--- src/components/ucp-tabs/config-editor/ImportButton.tsx
+++ src/components/ucp-tabs/config-editor/ImportButton.tsx
@@ -3,7 +3,7 @@ import { Folder, Folder2Open } from 'react-bootstrap-icons';
 function ImportButton(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className="ucp-button text-light"
+      className="ucp-button ucp-button--square text-light"
       type="button"
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}

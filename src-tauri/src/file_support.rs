021f8f044e6023e2bdb44a6bfe0b651070713ee3
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2023-12-03 21:55:50 +0100

    [FILES] improved matching options and read dir behaviour, now considering scope better

--- src-tauri/src/file_support.rs
+++ src-tauri/src/file_support.rs
@@ -1,20 +1,26 @@
 use path_slash::{PathBufExt, PathExt};
-use std::{borrow::Cow, ops::Deref, path::Path};
+use std::path::Path;
 use tauri::{
     api::dir::{read_dir, DiskEntry},
     scope::GlobPattern,
-    AppHandle,
+    AppHandle, FsScope, Manager,
 };
 
-use crate::utils::get_allowed_path_with_string_error;
+use crate::{constants::PATH_MATCH_OPTIONS, utils::get_allowed_path_with_string_error};
 
-fn fill_with_paths_with_slash(disk_entries: &Vec<DiskEntry>, fill_container: &mut Vec<String>) {
+fn fill_with_paths_with_slash(
+    fs_scope: &FsScope,
+    disk_entries: &Vec<DiskEntry>,
+    fill_container: &mut Vec<String>,
+) {
     for entry in disk_entries {
-        if let Some(path_string) = entry.path.to_slash() {
-            fill_container.push(path_string.to_string());
+        if fs_scope.is_allowed(&entry.path) {
+            if let Some(path_string) = entry.path.to_slash() {
+                fill_container.push(path_string.to_string());
+            }
         }
         if let Some(children) = &entry.children {
-            fill_with_paths_with_slash(children, fill_container);
+            fill_with_paths_with_slash(fs_scope, children, fill_container);
         }
     }
 }
@@ -26,21 +32,21 @@ pub async fn read_and_filter_dir(
     base: &str,
     pattern: &str,
 ) -> Result<Vec<String>, String> {
-    let source_path = get_allowed_path_with_string_error(&app_handle, base)?;
-    let path = dunce::canonicalize(source_path).map_err(|err| err.to_string())?;
+    let fs_scope = app_handle.fs_scope();
+    if !fs_scope.is_allowed(base) {
+        return Ok(vec![]);
+    }
+    let path = dunce::canonicalize(base).map_err(|err| err.to_string())?;
 
-    let found_entries = read_dir(&path, true).map_err(|err| err.to_string())?;
+    let found_entries = read_dir(path, true).map_err(|err| err.to_string())?;
     let mut found_paths = vec![];
-    fill_with_paths_with_slash(&found_entries, &mut found_paths);
+    fill_with_paths_with_slash(&fs_scope, &found_entries, &mut found_paths);
 
     if !pattern.is_empty() {
-        let complete_pattern_path = path.join(pattern);
-        let complete_pattern_path_with_slash = complete_pattern_path
-            .to_slash()
-            .unwrap_or_else(|| Cow::Borrowed(pattern));
-        let glob_pattern = GlobPattern::new(complete_pattern_path_with_slash.deref())
-            .map_err(|err| err.to_string())?;
-        found_paths.retain(|path_string: &String| glob_pattern.matches(path_string));
+        let glob_pattern = GlobPattern::new(pattern).map_err(|err| err.to_string())?;
+        found_paths.retain(|path_string: &String| {
+            glob_pattern.matches_with(path_string, PATH_MATCH_OPTIONS)
+        });
     }
     Ok(found_paths)
 }
@@ -60,8 +66,13 @@ pub async fn slashify(path: &str) -> Result<String, String> {
 
 // changes path to use slash as separator
 #[tauri::command]
-pub async fn canonicalize(path: &str, slash: bool) -> Result<String, String> {
-    let canonical_path = dunce::canonicalize(path).map_err(|err| err.to_string())?;
+pub async fn canonicalize(
+    app_handle: AppHandle,
+    path: &str,
+    slash: bool,
+) -> Result<String, String> {
+    let validated_path = get_allowed_path_with_string_error(&app_handle, path)?;
+    let canonical_path = dunce::canonicalize(validated_path).map_err(|err| err.to_string())?;
     if slash {
         slashify_path(&canonical_path)
     } else {

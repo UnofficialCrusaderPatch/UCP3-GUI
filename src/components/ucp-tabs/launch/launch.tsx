b91d6d5a33a5ad1e46e1e1167f1849b3a869bff3
gynt <gynt@users.noreply.github.com>
2024-01-01 22:07:02 +0100

    fix reversed logic for View Luanch Options

--- src/components/ucp-tabs/launch/launch.tsx
+++ src/components/ucp-tabs/launch/launch.tsx
@@ -64,7 +64,7 @@ export default function Launch() {
             setDisplayAdvancedLaunchOptions(!displayAdvancedLaunchOptions)
           }
         >
-          {displayAdvancedLaunchOptions
+          {!displayAdvancedLaunchOptions
             ? t('gui-launch:launch.options.view')
             : t('gui-launch:launch.options.hide')}
         </Button>

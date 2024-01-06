a639b1604f3a44f744cac811105b82aa0f67c48a
gynt <gynt@users.noreply.github.com>
2024-01-06 22:58:36 +0100

    pass 1 on deleting unused files

--- src/function/global/ArrayReducer.ts
+++
@@ -1,9 +0,0 @@
-import { ArrayReducerArgs, ArrayReducerState } from './types';
-
-// eslint-disable-next-line import/prefer-default-export
-export function ArrayReducer<Type>() {
-  return (
-    _state: ArrayReducerState<Type>,
-    newState: ArrayReducerArgs<Type>,
-  ) => [...newState];
-}

acc6ee77a978e8ee327388874cf2b444ca4ef2d1
gynt <gynt@users.noreply.github.com>
2024-01-10 22:33:55 +0100

    removed axios dependency

--- src/function/download/download-enums.ts
+++ src/function/download/download-enums.ts
@@ -1,6 +1,4 @@
 export const UCP_REPOS_USERNAME = 'ucp3-machine';
-export const UCP3_REPOS_MACHINE_TOKEN =
-  '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';
 
 export const UCP3_REPO_URL = 'UnofficialCrusaderPatch/UnofficialCrusaderPatch3';
 export const UCP3_REPO_URL_API = `https://api.github.com/repos/${UCP3_REPO_URL}`;
@@ -10,9 +8,8 @@ export const UCP3_REPO_GIST_URL =
 export const UCP_GUI_REPO_URL =
   'https://api.github.com/repos/UnofficialCrusaderPatch/UCP3-GUI/releases/tags/latest-tauri';
 
-export const GITHUB_BASIC_AUTH = `Basic ${window.btoa(
-  `${UCP_REPOS_USERNAME}:${UCP3_REPOS_MACHINE_TOKEN}`,
-)}`;
+export const GITHUB_BASIC_AUTH = (token: string) =>
+  `Basic ${window.btoa(`${UCP_REPOS_USERNAME}:${token}`)}`;
 
 export const GITHUB_AUTH_HEADER = {
   headers: {
--- src/function/download/github.ts
+++ src/function/download/github.ts
@@ -1,12 +1,7 @@
-import axios from 'axios';
 import { SemVer } from 'semver';
 import { ResponseType } from '@tauri-apps/api/http';
-import Logger, { ConsoleLogger } from '../../util/scripts/logging';
-import {
-  UCP3_REPOS_MACHINE_TOKEN,
-  UCP3_REPO_GIST_URL,
-  UCP3_REPO_URL_API,
-} from './download-enums';
+import Logger from '../../util/scripts/logging';
+import { UCP3_REPO_GIST_URL } from './download-enums';
 import { fetch } from '../../tauri/tauri-http';
 
 const LOGGER = new Logger('github.ts');
@@ -82,7 +77,7 @@ export class UCP3Updater {
   }
 
   async fetchUpdate() {
-    ConsoleLogger.info('fetchUpdate');
+    LOGGER.msg('fetchUpdate').info();
     if (this.meta === undefined) {
       this.meta = await this.fetchMeta();
     }
@@ -102,70 +97,3 @@ export class UCP3Updater {
     } as UpdateData;
   }
 }
-
-async function checkForLatestUCP3DevReleaseUpdate(
-  currentSHA: string,
-): Promise<{ update: boolean; file: string; downloadUrl: string }> {
-  const result = {
-    update: false,
-    file: '',
-    downloadUrl: '',
-  };
-  await axios
-    .get(`${UCP3_REPO_URL_API}/releases/tags/latest`, {
-      auth: { username: 'ucp3-machine', password: UCP3_REPOS_MACHINE_TOKEN },
-    })
-    .then(
-      (res: {
-        data: {
-          assets: {
-            browser_download_url: any;
-            name: string;
-            url: string;
-          }[];
-        };
-      }) => {
-        const devReleaseAsset = res.data.assets.filter(
-          (asset) => asset.name.indexOf('DevRelease') !== -1,
-        )[0];
-
-        const detectedSha = devReleaseAsset.browser_download_url
-          .split('UCP3-snapshot-DevRelease-')[1]
-          .split('.zip')[0];
-
-        if (!currentSHA.startsWith(detectedSha)) {
-          result.update = true;
-          result.file = devReleaseAsset.name;
-          result.downloadUrl = devReleaseAsset.url;
-        }
-
-        return result;
-      },
-    )
-    .catch((error: Error) => {
-      LOGGER.obj(error).error();
-      window.alert(error);
-    });
-
-  // TODO: there should be a proper "no/failed connection"-handling
-
-  return result;
-}
-
-export async function getLatestUCP3Artifacts() {
-  const result = await axios
-    .get(`${UCP3_REPO_URL_API}/actions/artifacts`, {
-      auth: { username: 'ucp3-machine', password: UCP3_REPOS_MACHINE_TOKEN },
-    })
-    .then((res: { data: { artifacts: unknown } }) => {
-      LOGGER.obj(res).error();
-      return res.data.artifacts;
-    })
-    .catch((error: Error) => {
-      LOGGER.obj(error).error();
-    });
-  return result;
-}
-
-// eslint-disable-next-line import/prefer-default-export
-export { checkForLatestUCP3DevReleaseUpdate, UCP3_REPOS_MACHINE_TOKEN };

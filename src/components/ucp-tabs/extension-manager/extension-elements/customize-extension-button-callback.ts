416cef94c515571474ad6b2c660941390715c33b
gynt <gynt@users.noreply.github.com>
2024-03-02 01:04:06 +0100

    add warning if exntesion modification starts before saving dirty config

--- src/components/ucp-tabs/extension-manager/extension-elements/customize-extension-button-callback.ts
+++ src/components/ucp-tabs/extension-manager/extension-elements/customize-extension-button-callback.ts
@@ -1,22 +1,56 @@
 import { Extension } from '../../../../config/ucp/common';
 import {
+  CONFIGURATION_FULL_REDUCER_ATOM,
   CONFIGURATION_QUALIFIER_REDUCER_ATOM,
   CONFIGURATION_TOUCHED_REDUCER_ATOM,
   CONFIGURATION_USER_REDUCER_ATOM,
+  UCP_CONFIG_FILE_ATOM,
 } from '../../../../function/configuration/state';
 import { ExtensionsState } from '../../../../function/extensions/extensions-state';
-import { EXTENSION_STATE_INTERFACE_ATOM } from '../../../../function/extensions/state/state';
+import {
+  EXTENSION_STATE_INTERFACE_ATOM,
+  EXTENSION_STATE_REDUCER_ATOM,
+} from '../../../../function/extensions/state/state';
 import { getStore } from '../../../../hooks/jotai/base';
 import { ConsoleLogger } from '../../../../util/scripts/logging';
 import { showModalOk } from '../../../modals/modal-ok';
+import { showModalOkCancel } from '../../../modals/modal-ok-cancel';
+import { makeToast } from '../../../toasts/toasts-display';
+import { CONFIG_DIRTY_STATE_ATOM } from '../../common/buttons/config-serialized-state';
 import { EXTENSION_EDITOR_STATE_ATOM } from '../../common/extension-editor/extension-editor-state';
 import { constructUserConfigObjects } from '../../common/import-button-callback';
+import saveConfig from '../../common/save-config';
 import { buildExtensionConfigurationDB } from '../extension-configuration';
 import { addExtensionToExplicityActivatedExtensions } from '../extensions-state-manipulation';
 
 // eslint-disable-next-line import/prefer-default-export
 export const customizeExtensionButtonCallback = async (ext: Extension) => {
   ConsoleLogger.debug('customizeExtensionButtonCallback');
+
+  if (getStore().get(CONFIG_DIRTY_STATE_ATOM)) {
+    const answer = await showModalOkCancel({
+      title: 'Lose unsaved changes?',
+      message:
+        'Customizing an extension will drop current unsaved changes to your config. Save your current changes?',
+    });
+
+    if (answer) {
+      const result: string = await saveConfig(
+        getStore().get(CONFIGURATION_FULL_REDUCER_ATOM),
+        getStore().get(CONFIGURATION_USER_REDUCER_ATOM),
+        getStore().get(UCP_CONFIG_FILE_ATOM), // `${getCurrentFolder()}\\ucp3-gui-config-poc.yml`,
+        getStore().get(EXTENSION_STATE_REDUCER_ATOM)
+          .explicitlyActivatedExtensions,
+        getStore().get(EXTENSION_STATE_REDUCER_ATOM).activeExtensions,
+        getStore().get(CONFIGURATION_QUALIFIER_REDUCER_ATOM),
+      );
+
+      makeToast({
+        title: result,
+        body: result,
+      });
+    }
+  }
 
   const oldExtensionsState = getStore().get(EXTENSION_STATE_INTERFACE_ATOM);
 

05965ba5dbd96cc172585c167eb2a70e41602489
TheRedDaemon <66257843+TheRedDaemon@users.noreply.github.com>
2024-03-24 22:59:29 +0100

    [LAUNCH_OPTIONS] add custom game data path launch options

---
+++ src/components/ucp-tabs/launch/launch-options/game-data-path.tsx
@@ -0,0 +1,123 @@
+import { useAtom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
+import { useTranslation } from 'react-i18next';
+import { TFunction } from 'i18next';
+import { TrashFill } from 'react-bootstrap-icons';
+import { useCurrentGameFolder } from '../../../../function/game-folder/utils';
+import { showModalOk } from '../../../modals/modal-ok';
+import { openFolderDialog } from '../../../../tauri/tauri-dialog';
+import { LaunchOptions } from './launch-options';
+
+interface GameDataPathMapping {
+  date: number;
+  gameFolder: string;
+  gameDataPath: string;
+}
+
+const UCP_GAME_DATA_PATH_ARG = '--ucp-game-data-path';
+
+const NUMBER_OF_PATH_MAPPINGS = 20;
+const GAME_DATA_PATH_MAP_KEY = 'gameDataPathMap';
+const GAME_DATA_PATH_MAP_STORAGE_ATOM = atomWithStorage(
+  GAME_DATA_PATH_MAP_KEY,
+  [] as GameDataPathMapping[],
+);
+
+function useGameDataStorageMap(
+  currentFolder: string,
+  t: TFunction,
+): [string | undefined, (path?: string) => void] {
+  const [mappingArray, setMappingArray] = useAtom(
+    GAME_DATA_PATH_MAP_STORAGE_ATOM,
+  );
+
+  const currentMappingIndex = mappingArray.findIndex(
+    (value) => currentFolder === value.gameFolder,
+  );
+  const currentMapping =
+    currentMappingIndex >= 0 ? mappingArray[currentMappingIndex] : undefined;
+  const isRecent = currentMappingIndex === mappingArray.length - 1;
+
+  return [
+    currentMapping?.gameDataPath,
+    (path?: string) => {
+      if (!path) {
+        if (currentMapping) {
+          mappingArray.splice(currentMappingIndex, 1);
+          setMappingArray([...mappingArray]);
+        }
+        return;
+      }
+
+      if (currentMapping) {
+        currentMapping.gameDataPath = path;
+        currentMapping.date = Date.now();
+        if (!isRecent) {
+          mappingArray.splice(currentMappingIndex, 1);
+          mappingArray.push(currentMapping);
+        }
+        setMappingArray([...mappingArray]);
+        return;
+      }
+
+      if (mappingArray.length + 1 > NUMBER_OF_PATH_MAPPINGS) {
+        const removedMapping = mappingArray.shift();
+
+        // no need to wait, is only confirm
+        showModalOk({
+          title: t('gui-launch:launch.options.game.data.path.gone.title'),
+          message: t('gui-launch:launch.options.game.data.path.gone.message', {
+            gameFolder: removedMapping?.gameFolder,
+          }),
+        });
+      }
+      mappingArray.push({
+        date: Date.now(),
+        gameFolder: currentFolder,
+        gameDataPath: path,
+      });
+      setMappingArray([...mappingArray]);
+    },
+  ];
+}
+
+export default function GameDataPath({ getArgs, setArgs }: LaunchOptions) {
+  const currentFolder = useCurrentGameFolder();
+  const [t] = useTranslation(['gui-launch']);
+  const [currentGameDataPath, setGameDataPath] = useGameDataStorageMap(
+    currentFolder,
+    t,
+  );
+
+  const currentArgs = getArgs();
+  const gamesDataPathArg = currentArgs.length > 0 ? currentArgs[1] : undefined;
+
+  if (currentGameDataPath !== gamesDataPathArg) {
+    setArgs(
+      currentGameDataPath ? [UCP_GAME_DATA_PATH_ARG, currentGameDataPath] : [],
+    );
+  }
+
+  return (
+    <div className="launch__options__box--game-data-path">
+      <h5>{t('gui-launch:launch.options.game.data.path')}</h5>
+      <div>
+        <input
+          type="text"
+          readOnly
+          value={currentGameDataPath ?? ''}
+          onClick={async () => {
+            const configPath = await openFolderDialog(
+              currentFolder,
+              t('gui-launch:launch.options.game.data.path.select.title'),
+            );
+            configPath.ifPresent(setGameDataPath);
+          }}
+        />
+        <button type="button" onClick={() => setGameDataPath()}>
+          <TrashFill />
+        </button>
+      </div>
+    </div>
+  );
+}

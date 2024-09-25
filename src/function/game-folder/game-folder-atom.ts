195e97f468cdd8a130b47a46c75ea7f9f8e11bd9
gynt <gynt@users.noreply.github.com>
2024-09-25 22:29:46 +0200

    docs

--- src/function/game-folder/game-folder-atom.ts
+++ src/function/game-folder/game-folder-atom.ts
@@ -1,4 +1,12 @@
 import { atom } from 'jotai';
 
+/**
+ * Holds the game folder (string) that is currently active.
+ * Generally this is also set on start to the previously active value.
+ *
+ * From a programming perspective, this is
+ * the entry point of the modding framework configuration logic.
+ * If this is changed, many update callbacks are called throughout the code.
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const GAME_FOLDER_ATOM = atom('');

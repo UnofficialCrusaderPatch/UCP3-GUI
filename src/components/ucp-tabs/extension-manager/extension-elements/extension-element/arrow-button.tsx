39a862be553aff0726f1c128e25f4457e90a5d69
Krarilotus <51748815+Krarilotus@users.noreply.github.com>
2026-09-11 00:51:30 +0200

    fix: keep family and extension controls visible in nested rows

--- src/components/ucp-tabs/extension-manager/extension-elements/extension-element/arrow-button.tsx
+++ src/components/ucp-tabs/extension-manager/extension-elements/extension-element/arrow-button.tsx
@@ -1,6 +1,7 @@
 import { useSetAtom } from 'jotai';
 import { MessageType } from '../../../../../localization/localization';
 import { STATUS_BAR_MESSAGE_ATOM } from '../../../../footer/footer';
+import { useMessage } from '../../../../general/message';
 
 // eslint-disable-next-line import/prefer-default-export
 export function ArrowButton(props: {
@@ -11,10 +12,12 @@ export function ArrowButton(props: {
 }) {
   const { clickCallback, disabled, buttonText, className } = props;
   const setStatusBarMessage = useSetAtom(STATUS_BAR_MESSAGE_ATOM);
+  const localize = useMessage();
   return (
     <button
       type="button"
       className={`fs-8 ${className}`}
+      aria-label={localize(buttonText)}
       onClick={clickCallback}
       disabled={disabled}
       onPointerEnter={() => {
